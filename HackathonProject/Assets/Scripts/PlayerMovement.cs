05e8159f7a52c95d021ce04a8bd4e04ffbe61e1d
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-08-06 14:33:49 +0300

    Pusht

---
+++ HackathonProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    [Header("Speed Settings")]
+    public float movementSpeed;
+    public float controlSpeed;
+    //public Quaternion rotateObj;
+    
+
+    //Touch settings
+    [Header("Touch Settings")]
+    [SerializeField] bool isTouching;
+    float touchPosX;
+    Vector3 direction;
+
+    //Animation
+    public Animator animator;
+    
+    //Not sure about this// Getting playerstate from gamemanager
+    public GameManager.PlayerState playerState;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        
+
+
+    }
+
+    private void Update()
+    {
+        //Make sure its in sync
+        playerState =  GameManager.inst.playerState ;
+
+        //Start game if in Playing State
+        if (playerState == GameManager.PlayerState.Playing)
+        {
+            GetInput();
+
+        }
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        //Start game if in Playing State
+        if (playerState == GameManager.PlayerState.Playing)
+        {
+            //RigidBody Eklentisinden sonra burası rigidbody olarak değişecek
+            //m_Rigidbody.AddForce(transform.forward * movementSpeed);
+            //transform.position += m_Rigidbody.AddForce(transform.forward * movementSpeed);
+            //transform.position += Vector3.forward * movementSpeed * Time.fixedDeltaTime;
+            m_Rigidbody.velocity = Vector3.forward * movementSpeed;
+           // animator.SetTrigger("GameStart"); // start the animation
+            //m_Rigidbody.AddForce(Vector3.forward * movementSpeed * Time.fixedDeltaTime);
+        
+            if (isTouching)
+            {
+                touchPosX = Input.GetAxis("Mouse X") * controlSpeed * Time.fixedDeltaTime;
+                
+                //Debug.Log(Input.GetAxis("Mouse X"));
+               // rotateObj = Quaternion.Euler(0,Input.GetAxis("Mouse X")* 100 ,0);
+                //rotateObj.y = Mathf.Clamp(rotateObj.y,-30,30);
+                //gameObject.transform.rotation = rotateObj;
+            }
+            else
+            {
+                touchPosX = 0;
+            }
+
+            transform.position += new Vector3(touchPosX, 0, 0);
+        }
+        
+    }
+    //Testing Inputs
+    void GetInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            isTouching = true;
+        }
+        else
+        {
+            isTouching = false;
+        }
+    }
+    //Mobile Inputs
+    void GetInputMobile()
+    {
+        if (Input.touchCount > 0)
+        {
+            isTouching = true;
+        }
+        else
+        {
+            isTouching = false;
+        }
+    }
+}
---
+++ HackathonProject/Assets/Scripts/_layer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject startPos;
+    public GameObject cube;
+    public bool isBoolRunning;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isBoolRunning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("FormShape"))
+        {
+            if (!isBoolRunning)
+            {
+                StartCoroutine(FlyCube(other.gameObject));
+                
+                StartCoroutine(ResetBool());
+            }
+
+        }
+    }
+
+    IEnumerator FlyCube(GameObject other)
+    {
+
+        
+        
+        int width = other.GetComponent<FormShapeInfo>().width;
+        int height = other.GetComponent<FormShapeInfo>().height;
+        int depth = other.GetComponent<FormShapeInfo>().depth;
+        bool merdiven = other.GetComponent<FormShapeInfo>().merdiven;
+
+        int p = 0;
+
+        for (int i = 0; i < height; i++)
+        {
+            
+            for (int j = p; j < depth; j++)
+            {
+                for (int d = 0; d < width; d++)
+                {
+                    if ( i ==  j&& merdiven)
+                    {
+                        //Merdiven bloklari gelicek
+                        GameObject shaper = Instantiate(cube, startPos.transform.position, Quaternion.Euler(45,0,0));
+                        shaper.transform.DOMove(new Vector3(0 + (d * 0.6f), -0.85f + (i * 0.6f), 43.42f + (j * 0.6f)), 1);
+                        shaper.transform.DOScale(new Vector3(0.5f, 0.5f, 0.5f),1);
+                        yield return new WaitForSeconds(0.03f);
+                        shaper.GetComponent<BoxCollider>().enabled = true;
+                        
+                    }
+                    else
+                    {
+                        GameObject shaper = Instantiate(cube, startPos.transform.position, Quaternion.identity);
+                        shaper.transform.DOMove(new Vector3(0 + (d * 0.5f), -0.85f + (i * 0.5f), 43.42f + (j * 0.6f)), 1);
+                        shaper.transform.DOScale(new Vector3(0.6f, 0.55f, 0.6f),1);
+                        yield return new WaitForSeconds(0.03f);
+                        shaper.GetComponent<BoxCollider>().enabled = true;
+                    }
+                    
+                    
+
+                    
+                }
+
+               
+
+            }
+
+            if (merdiven)
+            {
+                p++;
+
+            }
+
+            
+        }
+    }
+    
+    IEnumerator ResetBool()
+    {
+        isBoolRunning = true;
+
+        yield return new WaitForSeconds(5f);
+        isBoolRunning = false;
+    }
+}
