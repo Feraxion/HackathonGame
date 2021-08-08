2097b27f940d939318fb7128c5518c51f506bf3f
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-08-09 02:04:30 +0300

    Up

--- SugarDaddyRush/Assets/Scripts/PlayerSlider.cs
+++ SugarDaddyRush/Assets/Scripts/PlayerSlider.cs
@@ -15,7 +15,7 @@ public class PlayerSlider : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        loser = true;
     }
 
     // Update is called once per frame
